b1dabb5c449b20db59ec9350b5557e1df1182da8
Ada Cohen <ada@fieldbook.com>
2021-03-08 23:49:09 -0800

    Cell splitting

--- render/cell.go
+++ render/cell.go
@@ -1,3 +1,123 @@
 package render
 
-type Cell struct{}
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+
+	"github.com/osuushi/vivid/vivian"
+)
+
+type Cell struct {
+	MinWidth  int
+	MaxWidth  int
+	Wrap      bool
+	Alignment Alignment
+	Content   []vivian.Node
+}
+
+type Alignment byte
+
+const (
+	Left Alignment = iota
+	Right
+	Center
+	Justify
+)
+
+// Split an AST into cells. The AST must already be hoisted so that cell
+// creators live at the top level
+func cellsFromAst(hoistedAst *vivian.Ast) ([]*Cell, error) {
+	cells := []*Cell{}
+	currentCellNodes := []vivian.Node{}
+
+	// When we hit the end of a run of non-cell creators, they get bundled up into
+	// a default-configured cell.
+	aggregateTopLevelCellIfNeeded := func() {
+		if len(currentCellNodes) == 0 {
+			return
+		}
+		cell := makeDefaultCell()
+		cell.Content = currentCellNodes
+		cells = append(cells, cell)
+		currentCellNodes = []vivian.Node{}
+	}
+
+	for _, node := range hoistedAst.Content.Children {
+		if isCellCreatorNode(node) {
+			aggregateTopLevelCellIfNeeded()
+			cell, err := cellFromCellCreator(node)
+			if err != nil {
+				return nil, err
+			}
+			cells = append(cells, cell)
+		} else {
+			currentCellNodes = append(currentCellNodes, node)
+		}
+	}
+	aggregateTopLevelCellIfNeeded()
+
+	return cells, nil
+}
+
+func makeDefaultCell() *Cell {
+	return &Cell{}
+}
+
+func cellFromCellCreator(node vivian.Node) (*Cell, error) {
+	cell := makeDefaultCell()
+	var contentNode *vivian.ContentNode
+	for isCellCreatorNode(node) {
+		contentNode = node.(*vivian.ContentNode)
+		err := applyTag(contentNode.Tag, cell)
+		if err != nil {
+			return nil, err
+		}
+		node = contentNode.Children[0]
+	}
+	cell.Content = contentNode.Children
+	return cell, nil
+}
+
+var tagParsePattern *regexp.Regexp
+
+func init() {
+	tagParsePattern = regexp.MustCompile("(?i)([a-z]+)(\\d*)")
+}
+
+func applyTag(tag string, cell *Cell) error {
+	match := tagParsePattern.FindStringSubmatch(tag)
+	name := match[1]
+	// Only invalid parse is empty string, and it's fine for that to be zero
+	param64, _ := strconv.ParseInt(match[2], 10, 64)
+	param := int(param64)
+
+	switch name {
+	case "min":
+		cell.MinWidth = param
+	case "max":
+		cell.MaxWidth = param
+	case "wrap":
+		cell.Wrap = true
+		// Number parameter is shorthand for @wrap@fixedNN
+		cell.MinWidth = param
+		cell.MaxWidth = param
+	case "fixed":
+		cell.MinWidth = param
+		cell.MaxWidth = param
+	case "left":
+		// Pointless assignment, but for consistency
+		cell.Alignment = Left
+	case "right":
+		cell.Alignment = Right
+	case "center":
+		cell.Alignment = Center
+	case "justify":
+		cell.Alignment = Justify
+	default:
+		// Only possible to escape other cases via a bug. No other tag would have
+		// matched as a cell creator.
+		return fmt.Errorf("Unhandled cell creator type %s", name)
+	}
+	return nil
+}
---
+++ render/cell_test.go
@@ -0,0 +1,56 @@
+package render
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/go-test/deep"
+	"github.com/kr/pretty"
+	"github.com/osuushi/vivid/vivian"
+)
+
+func TestApplyTagAllCellCreatorsHandled(t *testing.T) {
+	// Ensure that no case panics in applyTag
+	for _, prefix := range cellCreatingNamePrefixes {
+		cell := makeDefaultCell()
+		err := applyTag(prefix, cell)
+		if err != nil && strings.HasPrefix(err.Error(), "Unhandled cell creator type") {
+			t.Error(err.Error())
+		}
+	}
+}
+
+func TestCellsFromAst(t *testing.T) {
+	ast, err := vivian.ParseString("hello @-val @max30[this is] @fixed50@green@right[a test]")
+	hoistCells(ast)
+
+	cells, err := cellsFromAst(ast)
+	if err != nil {
+		t.Error(err)
+	}
+	pretty.Println(cells)
+	// Clear content nodes, since we don't want to test those after this point
+	for _, cell := range cells {
+		cell.Content = nil
+	}
+
+	expected := []*Cell{
+		makeDefaultCell(),
+		&Cell{
+			MaxWidth: 30,
+		},
+		// Space by itself gets its own cell
+		// TODO: Should it? This should maybe be a special case, since the cell is
+		// effectively empty.
+		makeDefaultCell(),
+		&Cell{
+			MinWidth:  50,
+			MaxWidth:  50,
+			Alignment: Right,
+		},
+	}
+
+	if diff := deep.Equal(cells, expected); diff != nil {
+		t.Error("\n" + strings.Join(diff, "\n"))
+	}
+}
