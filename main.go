859b666d770120c913ec952ebe250da8e0805d4e
Ada Cohen <ada@fieldbook.com>
2021-03-14 00:02:07 -0800

    Measure perf in main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/osuushi/vivid/render"
 	"gopkg.in/yaml.v3"
@@ -36,11 +37,6 @@ func main() {
 		panic(err)
 	}
 
-	template, err := render.MakeRow(os.Args[2])
-	if err != nil {
-		panic(err)
-	}
-
 	width, err := render.TerminalWidth()
 	if err != nil {
 		panic(err)
@@ -48,17 +44,34 @@ func main() {
 
 	beam := render.DefaultBeam()
 
+	start := time.Now()
+	template, err := render.MakeRow(os.Args[2])
+	if err != nil {
+		panic(err)
+	}
+
+	elapsed := time.Since(start)
+	start = time.Now()
+
 	for _, context := range contexts {
 		rows, err := template.Render(width, beam, context)
 		if err != nil {
 			panic(err)
 		}
+		// Don't measure actual stdout time
+		elapsed += time.Since(start)
 		for _, row := range rows {
 			fmt.Println(row)
 		}
+		start = time.Now()
 	}
 
 	if err != nil {
 		panic(err)
 	}
+
+	elapsed += time.Since(start)
+
+	fmt.Println("Rendering time:", elapsed)
+	fmt.Println("Per row:", time.Duration(int64(elapsed)/int64(len(contexts))))
 }
